67c37dd129ffacd08b31b2e9e7b8e17b433d0ef0
singhbanta <singhbanta84@gmail.com>
2025-03-23 17:32:41 +0530

    fix: errors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.DATABASE_URL)
 .then(()=>{
     const PORT=3000;
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on ${PORT}`);
     });
 })
 .catch((err) => {
